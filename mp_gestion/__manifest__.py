dbfc4764389e569ae2cb7ce99dc3c04a61ec2422
Jhon Jairo Rojas Ortiz <jhon.rojas@nextpro.pe>
2024-03-04 04:40:46 +0000

    mas campos

--- mp_gestion/__manifest__.py
+++ mp_gestion/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "corner.store59@gmail.com",
     'category': 'account',
     'version': '15.0.0.0.1',
-    'depends': ['account', 'l10n_latam_invoice_document', 'payroll_payment'],
+    'depends': ['account', 'l10n_latam_invoice_document', 'payroll_payment', 'account_bank_cash_report'],
     'data': [
         # 'security/ir.model.access.csv',
         'security/mp_gestion_group.xml',
